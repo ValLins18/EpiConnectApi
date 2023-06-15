57b6520e98e3f2e761c5df53dbb0a1e56b81e699
ValLins18 <valdemilson.lins@gmail.com>
2023-06-15 18:37:04 -0300

    habilitando segurança

--- Program.cs
+++ Program.cs
@@ -22,37 +22,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddSwaggerGen(opt => {
-//    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "EPI-CONNECT_API", Version = "v1" });
-//    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
-//        Name = "Authorization",
+builder.Services.AddSwaggerGen(opt => {
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "EPI-CONNECT_API", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        Name = "Authorization",
 
-//        Description = "Authorization header using the Bearer scheme",
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer",
-//        BearerFormat = "JWT",
-//        In = ParameterLocation.Header
-//    });
-//    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-//                {
-//                    {
-//                          new OpenApiSecurityScheme
-//                          {
-//                              Reference = new OpenApiReference
-//                              {
-//                                  Type = ReferenceType.SecurityScheme,
-//                                  Id = "Bearer"
-//                              }
-//                          },
-//                         new string[] {}
-//                    }
-//                });
-//});
+        Description = "Authorization header using the Bearer scheme",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
 
 //builder.Services.AddSqlServer<AppDbContext>(builder.Configuration.GetConnectionString("EpiConnect"));
 builder.Services.AddDbContext<AppDbContext>(opt => {
-    opt.UseMySQL(builder.Configuration.GetConnectionString("EpiConnectMysql"));
-    //opt.UseSqlServer(builder.Configuration.GetConnectionString("EpiConnect"));
+    //opt.UseMySQL(builder.Configuration.GetConnectionString("EpiConnectMysql"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("EpiConnect"));
 });
 builder.Services.AddSignalR();
 
@@ -64,27 +64,27 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 
-//builder.Services.AddAuthorization(opt => {
-//    opt.FallbackPolicy = new AuthorizationPolicyBuilder()
-//    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme) //todos os endpoints necessitam de um token para serem acessados
-//    .RequireAuthenticatedUser()
-//    .Build();
-//});
-//builder.Services.AddAuthentication(authOpt => {
-//    authOpt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    authOpt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//})
-//    .AddJwtBearer(jwtOpt => {
-//        jwtOpt.TokenValidationParameters = new TokenValidationParameters() {
-//            ValidateActor = true,
-//            ValidateAudience = true,
-//            ValidateLifetime = true,
-//            ValidateIssuerSigningKey = true,
-//            ValidIssuer = builder.Configuration["JwtBearerTokenSettings:Issuer"],
-//            ValidAudience = builder.Configuration["JwtBearerTokenSettings:Audience"],
-//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtBearerTokenSettings:SecretKey"]))
-//        };
-//    });
+builder.Services.AddAuthorization(opt => {
+    opt.FallbackPolicy = new AuthorizationPolicyBuilder()
+    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme) //todos os endpoints necessitam de um token para serem acessados
+    .RequireAuthenticatedUser()
+    .Build();
+});
+builder.Services.AddAuthentication(authOpt => {
+    authOpt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    authOpt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(jwtOpt => {
+        jwtOpt.TokenValidationParameters = new TokenValidationParameters() {
+            ValidateActor = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtBearerTokenSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtBearerTokenSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtBearerTokenSettings:SecretKey"]))
+        };
+    });
 
 IMapper mapper = MapConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
@@ -105,7 +105,7 @@ var app = builder.Build();
     app.UseSwaggerUI();
 
 app.UseCors("AllowAnyOrigin");
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 #pragma warning disable ASP0014 // Suggest using top level route registrations
 app.UseRouting();
@@ -115,7 +115,7 @@ app.UseEndpoints(endpoints => {
     endpoints.MapHub<WebSocketHub3>("/websocket9");
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
-//app.UseAuthentication();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
