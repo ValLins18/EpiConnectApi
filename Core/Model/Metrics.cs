a6d2577d09aa92c13a7daef419ac6960b7c5516a
ValLins18 <valdemilson.lins@gmail.com>
2023-07-17 19:32:14 -0300

    [Correção#10] Mudar o tipo de variavel do Ruido para aceitar numeros decimais

--- Core/Model/Metrics.cs
+++ Core/Model/Metrics.cs
@@ -2,7 +2,7 @@
     public class Metrics {
         public int MetricsId { get; set; }
         public int BatteryLevel { get; set; }
-        public int? Noise{ get; set; }
+        public decimal? Noise{ get; set; }
         public bool IsProtected { get; set; }
         public bool IsContingency { get; set; } = false;
     }
--- Core/ViewModel/MetricsRequestView.cs
+++ Core/ViewModel/MetricsRequestView.cs
@@ -1,7 +1,7 @@
 ﻿namespace EpiConnectAPI.Core.ViewModel {
     public class MetricsRequestView {
         public int BatteryLevel { get; set; }
-        public int? Noise { get; set; }
+        public decimal? Noise { get; set; }
         public bool IsProtected { get; set; }
         public bool IsContingency { get; set; } = false;
     }
